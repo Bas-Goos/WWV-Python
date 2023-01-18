d6fa813f83bb01bcf5e667932ce2024e9af9fe9f
Bas-Goos <122937601+Bas-Goos@users.noreply.github.com>
2023-01-18 09:33:36 +0100

    Som van 3 getallen

---
+++ Module 2/Som van 3 getallen.py
@@ -0,0 +1,7 @@
+getal1 = int(input("Geef je eerste getal:   "))
+getal2 = int(input("Geef je tweede getal:   "))
+getal3 = int(input("Geef je derde getal:   "))
+
+som = getal1 + getal2 + getal3
+
+print(som)
