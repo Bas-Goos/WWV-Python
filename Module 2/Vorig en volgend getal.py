96a82245a4eabfdd0c17ab36090e700da116e5f0
Bas-Goos <122937601+Bas-Goos@users.noreply.github.com>
2023-01-18 10:04:02 +0100

    vorig en volgend

---
+++ Module 2/Vorig en volgend getal.py
@@ -0,0 +1,8 @@
+def vorigEnVolgend():
+
+    invoerWaarde = int(input("Geef een getal:   "))
+
+    print(f"Het getal links van het getal {str(invoerWaarde)} is het getal {str(invoerWaarde - 1)}.")
+    print(f"Het getal rechts van het getal {str(invoerWaarde)} is het getal {str(invoerWaarde + 1)}.")
+
+vorigEnVolgend()
