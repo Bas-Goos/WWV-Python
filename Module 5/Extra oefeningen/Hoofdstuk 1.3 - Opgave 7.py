85e14028fc7956605a3c04ff059a0ec81a61b768
Bas-Goos <122937601+Bas-Goos@users.noreply.github.com>
2023-03-01 08:28:37 +0100

    Create Hoofdstuk 1.3 - Opgave 7.py

---
+++ Module 5/Extra oefeningen/Hoofdstuk 1.3 - Opgave 7.py
@@ -0,0 +1,8 @@
+def celsiusNaarFahrenheit():
+    celsius = input("Geef een termperatuur in graden Celsius:   ")
+
+    fahrenheit = ((9/5)*celsius+32)
+
+    print(fahrenheit)
+
+celsiusNaarFahrenheit()
